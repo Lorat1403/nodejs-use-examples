5660f468b4f837283061e11acdc06aca7b34a8f6
Lorat1403 <lorat1403@gmail.com>
2025-08-18 17:01:50 +0300

    first variant

--- models/api/contacts.js
+++ models/api/contacts.js
@@ -1,17 +1,62 @@
-const listContacts = async () => {};
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
 
-const getContactById = async (contactId) => {};
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const removeContact = async (contactId) => {};
-
-const addContact = async (body) => {};
-
-const updateContact = async (contactId, body) => {};
-
-export {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+export const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
 };
+
+export const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+
+  return result || null;
+};
+
+export const updateContact = async (contacts) =>
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+export const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await updateContact(contacts);
+  return result;
+};
+
+export const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await updateContact(contacts);
+  return newContact;
+};
+
+// const listContacts = async () => {};
+
+// const getContactById = async (contactId) => {};
+
+// const removeContact = async (contactId) => {};
+
+// const addContact = async (body) => {};
+
+// const updateContact = async (contactId, body) => {};
+
+// export {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,41 @@
 import express from "express";
 
+import ctrlWrapper from "../../helpers/ctrlWrapper.js";
+import validateBody from "../../middlewares/validateBody.js";
+
+import addSchema from "../../schemas/contact.js";
+import * as ctrl from "../../controllers/contacts.js";
+
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+// router.get("/", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
 
-router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.get("/", ctrlWrapper(ctrl.getAll));
 
-router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+// router.get("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.get("/:id", ctrlWrapper(ctrl.getById));
 
-router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+// router.post("/", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+router.post("/", validateBody(addSchema), ctrlWrapper(ctrl.add));
+
+// router.delete("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+router.delete("/:id", ctrlWrapper(ctrl.removeById));
+
+// router.put("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+router.put("/:id", validateBody(addSchema), ctrlWrapper(ctrl.updateById));
 
 export default router;
